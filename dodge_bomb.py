417f1bb4fb96d3f5cc8e7f326e95ee153af6ddcf
tanaka <c0c2504419@edu.teu.ac.jp>
2026-04-21 14:08:49 +0900

    演習２:時間で爆弾が拡大、加速

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,7 +5,14 @@ import random
 import time
 
 WIDTH, HEIGHT = 1100, 650
+DELTA = {
+        pg.K_UP:(0,-5),
+        pg.K_DOWN:(0, +5),
+        pg.K_LEFT:(-5,0),
+        pg.K_RIGHT:(+5,0),
+}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 
 def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]:
     """
@@ -19,6 +26,7 @@ def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]:
     if obj_rct.top < 0 or HEIGHT< obj_rct.bottom: # 縦方向判定
         tate = False
     return yoko, tate
+
 
 def gameover(screen: pg.Surface) -> None:
     black_img = pg.Surface((WIDTH, HEIGHT))
@@ -43,6 +51,18 @@ def gameover(screen: pg.Surface) -> None:
     pg.display.update()
     time.sleep(5)
 
+
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    bb_accs = [a for a in range(1, 11)]
+    bb_imgs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_imgs.append(bb_img)      
+    return bb_imgs, bb_accs
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -52,19 +72,13 @@ def main():
     kk_rct.center = 300, 200
 
     bb_img= pg.Surface((20, 20))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH),random.randint(0, HEIGHT)
     vx = +5
     vy = +5
-
-    DELTA = {
-        pg.K_UP:(0,-5),
-        pg.K_DOWN:(0, +5),
-        pg.K_LEFT:(-5,0),
-        pg.K_RIGHT:(+5,0),
-    }
+    bb_imgs, bb_accs = init_bb_imgs()
 
     clock = pg.time.Clock()
     tmr = 0
@@ -77,9 +91,17 @@ def main():
         if kk_rct.colliderect(bb_rct):
             gameover(screen)
             return
-        screen.blit(bg_img, [0, 0]) 
+ 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
         for key, mv in DELTA.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
@@ -94,8 +116,16 @@ def main():
         if not tate:
             vy *= -1
 
+        avx= vx * bb_accs[min(tmr//500, 9)]
+        avy = vy * bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]        
+        bb_rct.width = bb_img.get_width()
+        bb_rct.height = bb_img.get_height()
+        bb_rct.move_ip(avx, avy)
+
+        screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx,vy)
+        #bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
