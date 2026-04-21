0870d31cc8e5ae39c6cef87f79f7e6ccc970e04b
tanaka <c0c2504419@edu.teu.ac.jp>
2026-04-21 11:08:24 +0900

    練習３:こうかとんと爆弾が画面の外に出ないようにした

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,6 +6,18 @@ import random
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]:
+    """
+    引数：こうかとんRectかばくだんRect
+    戻り値：タプル（横方向判定結果，縦方向判定結果）
+    画面内ならTrue，画面外ならFalse
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH< obj_rct.right: # 横方向判定
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT< obj_rct.bottom: # 縦方向判定
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -14,22 +26,25 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
     bb_img= pg.Surface((20, 20))
     pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
     bb_img.set_colorkey((0, 0, 0))
     bb_rct = bb_img.get_rect()
-    bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    bb_rct.center = random.randint(0, WIDTH),random.randint(0, HEIGHT)
     vx = +5
     vy = +5
-    clock = pg.time.Clock()
+
     DELTA = {
         pg.K_UP:(0,-5),
         pg.K_DOWN:(0, +5),
         pg.K_LEFT:(-5,0),
         pg.K_RIGHT:(+5,0),
     }
+
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -42,6 +57,15 @@ def main():
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
+        
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
         screen.blit(bb_img, bb_rct)
